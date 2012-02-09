43688a74ca194406b7b07d46d53a4749eb55879a
Nemec <djnemec@gmail.com>
2012-02-09 10:32:18 -0600

    First commit.

---
+++ src/ec/nem/BlueNetActivity.java
@@ -0,0 +1,13 @@
+package ec.nem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BlueNetActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
